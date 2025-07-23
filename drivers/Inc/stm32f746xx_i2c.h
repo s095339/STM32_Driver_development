f7b61d1b362f9b426689f738dbeb4c165ce03cf5
Joe <s095339@gmail.com>
2025-07-23 10:10:28 +0800

    non_blocking api

--- drivers/Inc/stm32f746xx_i2c.h
+++ drivers/Inc/stm32f746xx_i2c.h
@@ -37,6 +37,17 @@ typedef struct
 {
     I2C_RegDef_t    *pI2Cx;
     I2C_Config_t    I2C_Config;
+
+    //Non-blocking API
+    uint8_t         *pTxBuffer;          // To store the app. Tx buffer address
+    uint8_t         *pRxBuffer;          // To store the app. Rx buffer address
+    uint32_t        TxLen;              // To store Tx len
+    uint32_t        RxLen;              // To store Rx len
+    uint8_t         TxRxState;           // To store communication state @TxRxState
+    uint8_t         DevAddr;             // To store slave/device address
+    uint32_t        RxSize;             // To store Rx size
+    uint8_t         Sr;                  // To store repeated start value
+
 }I2C_Handle_t;
 
 /*
@@ -121,8 +132,8 @@ void I2C_ControllerReceiveData(
     I2C_Handle_t *pI2CHandle, uint8_t *pRxbuffer, uint32_t Len, uint8_t SlaveAddr, uint8_t AUTOEND
 );
 //non-blocking(Interrupt-based)
-uint8_t I2C_SendDataIT(I2C_Handle_t *pI2CHandle, uint8_t *pTxBuffer, uint32_t len);
-uint8_t I2C_ReceiveDataIT(I2C_Handle_t *pI2CHandle, uint8_t *pRxBuffer, uint32_t len);
+uint8_t I2C_ControllerSendDataIT(I2C_Handle_t *pI2CHandle,uint8_t *pTxBuffer, uint32_t Len,uint8_t SlaveAddr,uint8_t Sr);
+uint8_t I2C_ControllerReceiveDataIT(I2C_Handle_t *pI2CHandle,uint8_t *pRxBuffer, uint32_t Len, uint8_t SlaveAddr,uint8_t Sr);
 
 
 /*
