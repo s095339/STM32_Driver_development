d8da3c99bfbab552906e11d857c490582ef75783
Joe <s095339@gmail.com>
2025-07-23 10:11:59 +0800

    interrupt configuring

--- drivers/Src/stm32f746xx_i2c.c
+++ drivers/Src/stm32f746xx_i2c.c
@@ -331,17 +331,67 @@ void I2C_ApplicationEventCallback(I2C_Handle_t *pI2CHandle, uint8_t AppEv);
 /*
 ISR handling
 */
-void I2C_IRQITConfig(uint8_t IRQNumber, uint8_t EnorDi);
-void I2C_IRQPriorityConfig(uint8_t IRQNumber, uint8_t IRQPriority);
-
-
-
-
-//private function
-static inline void I2C_GenerateStartCondition(I2C_RegDef_t *pI2Cx)
+void I2C_IRQITConfig(uint8_t IRQNumber, uint8_t EnorDi)
 {
-    pI2Cx->CR2 |= 1 << I2C_CR2_START;
+    /*refer to the ARM® Cortex®-M7 Devices Generic user guide
+    4.2 NVIC 
+    這邊已經不是micro controlller side 已經接觸到processor side了 所以要去看processer的文件
+    以便設定NVIC*/    
+    
+    
+    /****
+     * 我們是要去設定processor的 Interrupt Set-enable Registers(ISER) 來打開interrupt
+     *  Interrupt Clear-enable Registers(ICER) 來關掉interrupt
+     */
+    if(EnorDi == ENABLE)
+    {
+        if(EnorDi == ENABLE)
+        {
+            if(IRQNumber <= 31)
+            {
+                //program ISER0 register
+                *NVIC_ISER0 |= (1<<IRQNumber);
+
+            }else if(IRQNumber>31 && IRQNumber<64)
+            {
+                *NVIC_ISER1 |= (1<<IRQNumber%32);
+            }else if(IRQNumber>64 && IRQNumber<96)//stm32f746g的interrupt number沒有那麼多
+            {
+                *NVIC_ISER2 |= (1<<IRQNumber%64);
+            }
+        }else{
+            if(IRQNumber <= 31)
+            {
+                //program ISER0 register
+                *NVIC_ICER0 &= ~(1<<IRQNumber);
+
+            }else if(IRQNumber>31 && IRQNumber<64)
+            {
+                *NVIC_ICER1 &= ~(1<<IRQNumber%32);
+            }else if(IRQNumber>64 && IRQNumber<96)//stm32f746g的interrupt number沒有那麼多
+            {
+                *NVIC_ICER2 &= ~(1<<IRQNumber%64);
+            }
+        }
+    }
 }
+void I2C_IRQPriorityConfig(uint8_t IRQNumber, uint8_t IRQPriority)
+{
+    //1. first lets find out the ipr register
+    // M7 user guide 4.2,  Interrupt Priority Registers
+    uint8_t iprx = IRQNumber;
+    uint8_t iprx_section = IRQNumber %4;
+    uint8_t shift_amount = (8*iprx_section) + (8-NO_PR_BITS_IMPLEMENTED);
+    /*根據UM 10.1 NVIC features 雖然cortex-M7的processor提供了8bits的中段優先級，
+    但是STM32的這個microprocessor只implement了4bits，所以要把IRQPriority右移4bit寫進去*/
+
+    *(NVIC_PR_BASE_ADDR + iprx) |= (IRQPriority << shift_amount);
+}
+
+
+
+
+
 
 static uint32_t RCC_GetI2CCLKValue(I2C_Handle_t *I2C_Handle)
 {
