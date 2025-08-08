49c4cde986fda37f61c64516a4a53f73a5ca018e
Joe <s095339@gmail.com>
2025-08-08 12:52:00 +0800

    systemcoreclock define and priortygrouping setting implementation

--- drivers/Src/stm32f746xx.c
+++ drivers/Src/stm32f746xx.c
@@ -7,7 +7,7 @@
 
 
 #include "stm32f746xx.h"
-
+uint32_t SystemCoreClock = 16000000;
 void delay_ms(uint32_t ms)
 {
     uint32_t start = DWT->CYCCNT;
@@ -22,4 +22,12 @@ void delay_us(uint32_t us)
     uint32_t cycles = us * (SystemCoreClock / 1000000);
 
     while ((DWT->CYCCNT - start) < cycles);
+}
+void NVIC_SetPriorityGrouping_Manual(uint32_t prigroup) {
+    uint32_t reg;
+
+    reg = SCB_AIRCR;
+    reg &= ~(SCB_AIRCR_VECTKEY_MASK | SCB_AIRCR_PRIGROUP_MASK); // 清除舊值
+    reg |= SCB_AIRCR_VECTKEY | (prigroup << 8);
+    SCB_AIRCR = reg;
 }
